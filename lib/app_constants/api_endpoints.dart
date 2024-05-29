b1ef1bfa0ff00aa0cc344fc652217d3f56b7b7ce
chintank8080 <chintank8080@gmail.com>
2024-05-29 22:59:51 +0530

    endpoints setup

--- lib/app_constants/api_endpoints.dart
+++ lib/app_constants/api_endpoints.dart
@@ -1,1 +1,6 @@
-class ApiEndpints {}
+class ApiEndpints {
+  static const baseUrl = "https://poolit.theclosedoor.com/api/";
+  static const sendOtp = "${baseUrl}sendOtpToPhone";
+  static const verifyOtp = "${baseUrl}login";
+  static const childrenList = "${baseUrl}children";
+}
